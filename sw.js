1a70061574a16165f72d328ada22a899d8188982
windmeal1-cmyk <windmeal1@gmail.com>
2026-07-07 12:20:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "coyg-v6";
+const CACHE = "coyg-v7";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => {
   e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
