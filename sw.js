9faba330677bdb4466b8a2aba91f5c1cb3a6ffd1
windmeal1-cmyk <windmeal1@gmail.com>
2026-07-07 13:38:38 +0900

    sw.js 업데이트

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "coyg-v11";
+const CACHE = "coyg-v12";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => {
   e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
